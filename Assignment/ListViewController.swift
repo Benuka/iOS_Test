6d9d75d82c9d3de06e4a3fd774d56f6958822244
Ahmed Ifhaam <ahmedifhaam1@gmail.com>
2020-04-25 22:33:50 +0530

    Fix for json

--- Assignment/ListViewController.swift
+++ Assignment/ListViewController.swift
@@ -8,29 +8,36 @@
 
 import UIKit
 
-class ListViewController: UIViewController {
+class ListViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
     
     var name = ["fefevf","feev","fefefe","fwevev"]
     var hotels = [HotelData]()
     
-    @IBOutlet weak var tableView: UITableView!
+    class FullResponse : Decodable{
+        let data : [HotelData]
+    }
+    
+    @IBOutlet var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "ListViewCell")
         // Do any additional setup after loading the view.
-        downloadJSON {
+        /*downloadJSON {
             print("Successful")
-        }
+        }*/
     }
     
     func downloadJSON(completed: @escaping () -> ()){
-        let url = URL(string: "https://dl.dropboxusercontent.com/s/6nt7fkdt7ck0lue/hotels")
-        URLSession.shared.dataTask(with: url!) { (data, response, error) in
+        let urlstring = "https://dl.dropboxusercontent.com/s/6nt7fkdt7ck0lue/hotels";
+        let url = URL(string:urlstring)!
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
             
             if error == nil {
                 do{
-                    self.hotels = try JSONDecoder().decode([HotelData].self, from: data!)
+                    let a = try JSONDecoder().decode(FullResponse.self, from: data!)
+                    self.hotels = a.data;
                     print(self.hotels.count)
                     
                     DispatchQueue.main.async {
@@ -43,20 +50,34 @@ class ListViewController: UIViewController {
             
         }.resume()
     }
-
-}
-
-extension ViewController: UITableViewDelegate, UITableViewDataSource{
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        var name = ["fefevf","feev","fefefe","fwevev"]
-        return name.count
-    }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ListViewCell
-        var name = ["fefevf","feev","fefefe","fwevev"]
-        cell?.nameLbl.text = name[indexPath.row]
-        return cell!
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ListViewCell", for: indexPath)// as! ListViewCell;
+        cell.textLabel?.text = "ifhaam";
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //var name = ["fefevf","feev","fefefe","fwevev"]
+        return 5
+    }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
 
 }
+
+//extension ViewController: UITableViewDelegate,UITableViewDataSource{
+//
+//
+//    /*func tableView(_ tableView: UITableView, cellForRowAt IndexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ListViewCell
+//        var name = ["fefevf","feev","fefefe","fwevev"]
+//        cell?.nameLbl.text = name[indexPath.row]
+//        return cell!
+//    }*/
+//
+//
+//
+//}
